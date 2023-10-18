4a3ccff7f6001c4d0cfdd5a4bd351f5a3f2a0e5e
Rukshan Fernando <fernandownr.21@uom.lk>
2023-10-19 01:58:38 +0530

    fixed image not seen error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+const nextConfig = {
+  images: {
+    domains: ["cdn.imagin.studio"],
+  },
+};
 
 module.exports = nextConfig;
