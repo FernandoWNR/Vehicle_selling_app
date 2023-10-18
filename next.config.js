1a3c41fd9a9ad2e2a8fc3a29942cc16f7437dea9
Rukshan Fernando <fernandownr.21@uom.lk>
2023-10-19 02:23:26 +0530

    add my-loader.ts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    domains: ["cdn.imagin.studio"],
+    loader: "custom",
+    loaderFile: "./my-loader.ts",
   },
 };
 
